2e38d58bda456396e17cb87fc145697c2f569908
PSP25 <priyanshupanda2019@gmail.com>
2024-06-26 01:59:41 +0530

    bug fix

--- Portfolio/src/pages/Iframe.jsx
+++ Portfolio/src/pages/Iframe.jsx
@@ -5,7 +5,7 @@ function Iframe() {
     <iframe
     title="Resume"
     className="w-full h-full"
-    src="https://drive.google.com/file/d/1Clnp9CFOCcbrEHH0biUCH1TgjQVkvphw/preview"
+    src="https://drive.google.com/file/d/1aSsG-sojTNgqIahgaqgusZBufAf3PiY8/preview"
     frameBorder="0"
     allowFullScreen
   ></iframe>
